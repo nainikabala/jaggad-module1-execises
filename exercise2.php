367926bde6d1a1f16ae63b0fbd2f77b1cc450891
balakumaran-naini <105928535+balakumaran-naini@users.noreply.github.com>
2023-04-26 18:03:20 +0200

    i added

---
+++ exercise2.php
@@ -0,0 +1,11 @@
+<?php
+
+$input1 = 7;
+$input2 = 2;
+$input3 = 5;
+
+if($input2<$input1){
+    echo $input2;
+} elseif ($input2<$input3){
+    echo $input2;
+}
---
+++ exercise3.php
@@ -0,0 +1,10 @@
+<?php
+
+$input = readline('input:');
+
+if($input % 2 == 0){
+    echo $input. 'This is an even number';
+} elseif($input % 2 != 0) {
+    echo $input. 'This is an odd number';
+}
+
